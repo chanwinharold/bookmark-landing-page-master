bda557a7a3be70570c8e61d287e39f8aa5b0bf10
chanwinharold <chanwinharold@gmail.com>
2025-08-07 20:11:16 +0200

    ajout d'animations à la section faq

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -8,9 +8,11 @@ function Faq({object}) {
         <div onClick={() => setFolded(value => !value)} className="faq-container">
             <div className="faq-head">
                 <h4>{object.title}</h4>
-                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="12"><path fill="none" stroke="#5267DF" strokeWidth="3" d="M1 1l8 8 8-8"/></svg>
+                <svg className={folded && "faq-arrow"} xmlns="http://www.w3.org/2000/svg" width="18" height="12"><path fill="none" stroke="#5267DF" strokeWidth="3" d="M1 1l8 8 8-8"/></svg>
             </div>
-            { folded ? <p>{object.paragraph}</p> : null }
+            <div className={!folded && "faq-paragraph"}>
+                <p>{object.paragraph}</p>
+            </div>
         </div>
     )
 }
